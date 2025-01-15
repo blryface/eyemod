8de4070e68ab6fa5742a30fe653396012b2193ed
blryface <me@blurry.gay>
2025-01-15 05:23:24 -0300

    feat: fabric template mod

---
+++ src/client/java/gay/blurry/eyemod/EyemodDataGenerator.java
@@ -0,0 +1,11 @@
+package gay.blurry.eyemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EyemodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
