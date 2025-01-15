484f5d9dae636f52d6de00d5693b5383e73f1526
blryface <me@blurry.gay>
2025-01-15 05:56:45 -0300

    feat: many small things

--- src/main/java/gay/blurry/eyemod/Eyemod.java
+++ src/main/java/gay/blurry/eyemod/Eyemod.java
@@ -8,9 +8,13 @@ import org.slf4j.LoggerFactory;
 public class Eyemod implements ModInitializer {
 	public static final String MOD_ID = "eyemod";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("You just lost the game!");
+	}
+}y, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
