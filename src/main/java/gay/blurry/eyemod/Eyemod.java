e8171c8cba17c69d85b83583ff9bb83b08211a9c
blryface <124838505+blryface@users.noreply.github.com>
2025-01-15 17:34:36 -0300

    fix: ACTUALLY remove duplicate code and missed bit of comment

--- src/main/java/gay/blurry/eyemod/Eyemod.java
+++ src/main/java/gay/blurry/eyemod/Eyemod.java
@@ -14,15 +14,4 @@ public class Eyemod implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("You just lost the game!");
 	}
-}y, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
 }
